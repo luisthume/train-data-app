cc5d65fe1a0d82b1393ee3424554a54602b0fa64
Thumé <luisltcs@gmail.com>
2024-12-14 18:48:17 +0000

    add health router to app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from controllers.train_controller import router as train_router
+from controllers.health_controler import router as health_router
 
 app = FastAPI(title="Digitraffic Kafka Pipeline")
 
 app.include_router(train_router, prefix="/train", tags=["Train Data"])
+app.include_router(health_router, tags=["Health Check"])
